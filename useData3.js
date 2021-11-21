c6599c0772e379d2ee18d0e94754d6d36d1120cd
blessedaim <93271813+blessedaim@users.noreply.github.com>
2021-11-21 17:12:41 +0300

    Update and rename useDataNew.js to useData3.js

--- useData3.js
+++ useData3.js
@@ -1,5 +1,5 @@
-import { React } from 'https://cdnjs.cloudflare.com/ajax/libs/react/17.0.2/umd/react.production.min.js';
-import { d3 } from 'https://cdnjs.cloudflare.com/ajax/libs/d3/7.1.1/d3.min.js';
+import React from 'react';
+import d3 from 'd3';
 
 const csvUrl =
   "https://gist.githubusercontent.com/blessedaim/194e3ffc1852220dcd762edf4d9768c0/raw/49bc369907064b312e0c235f112162d9dc9f31b0/UN_Population_2019.csv";
