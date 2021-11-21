65e0194ceb3218de7f2e2ede8a0f362164d70f25
blessedaim <93271813+blessedaim@users.noreply.github.com>
2021-11-21 17:38:42 +0300

    Create useDaata.js

---
+++ useDaata.js
@@ -0,0 +1,20 @@
+
+import { useState, useEffect } from 'https://cdnjs.cloudflare.com/ajax/libs/react/17.0.2/umd/react.production.min.js';
+import { csv } from 'https://cdnjs.cloudflare.com/ajax/libs/d3/7.1.1/d3.min.js';
+
+const csvUrl =
+  "https://gist.githubusercontent.com/blessedaim/194e3ffc1852220dcd762edf4d9768c0/raw/49bc369907064b312e0c235f112162d9dc9f31b0/UN_Population_2019.csv";
+
+export const useData = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const row = (d) => {
+      d.Population = +d["2020"];
+      return d;
+    };
+   csv(csvUrl, row).then((data) => {
+      setData(data.slice(0, 10));
+    });
+  }, []);
+  return data;
+}
